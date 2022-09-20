ca39d0a330881039f0c0bd61bc27605f44e01056
Muthukrthika <muthukrthika@gmail.com>
2022-09-20 14:53:17 +0400

    Add files via upload

---
+++ word_counter.js
@@ -0,0 +1,32 @@
+class WordCounter{
+    constructor(inputText){
+        this.inputText = inputText;
+        this.inputText.addEventListener('input', () => {
+            this.count();
+        });
+    }
+    count(){
+        let wordStat = this.getWordStat(this.inputText.value.trim());
+        this.emitEvent(wordStat)
+    }
+    emitEvent(wordStat){
+        let countEvent = new CustomEvent('count' , {
+            bubbles: true,
+            cancelable: true,
+            detail: {
+                wordStat
+            }
+        });
+
+        this.inputText.dispatchEvent(countEvent);
+
+    }
+    getWordStat(str){
+        let matches = str.match(/\S+/g);
+        return{
+            characters: str.length,
+            words: matches ? matches.length : 0,
+        };
+    }
+
+}
